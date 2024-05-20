005313c82fb992163022171093add72ec7f5c812
Kikayoung <135994708+Kikayoung@users.noreply.github.com>
2024-05-20 12:34:45 +0900

    commit

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var mysql = require("mysql");
 
+// var conn = mysql.createConnection({
+//     host: "210.117.212.134",
+//     user: "guro",
+//     password: "1234",
+//     database: "test",
+//     dateStrings: 'date'
+// });
+
+
 var conn = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -40,6 +49,7 @@ conn.connect((err) => {
 
 const express = require('express');
 const app = express();
+const sha=require('sha256');
 
 //body-parser라이브러리 추가
 const bodyParser = require('body-parser');
@@ -270,8 +280,11 @@ app.post('/save4', function(req, res) {
 //     res.send('도서 목록 관련 페이지입니다.');
 // });
 
+// app.get('/', function(req, res) {
+//     res.render('index.ejs')
+// });
 app.get('/', function(req, res) {
-    res.render('index.ejs')
+    res.render('index', { user: req.user });
 });
 
 app.get('/profile', function(req, res) {
@@ -284,4 +297,135 @@ app.get('/profile', function(req, res) {
 //     console.log(rows);
 // });
 
+
+
 app.use(express.static('public'));
+
+
+//쿠키쿠키
+let cookieParser = require('cookie-parser');
+
+
+app.use(cookieParser('ncvka0e398423kpfd'));
+// app.get('/cookie',function(req,res){
+//     let milk=parseInt(req.signedCookies.milk)+1000;
+//     if(isNaN(milk)){
+//         milk=0;
+//     }
+//     res.cookie('milk',milk, {signed:true});
+//     res.send('product: '+milk+'원');
+// });
+
+let session = require('express-session');
+
+
+app.use(session({
+    secret: 'dkufe8938493j4e08349u',
+    resave: false,
+    saveUninitialized: true
+}));
+
+// app.get('/session', function(req, res) {
+//     if (isNaN(req.session.milk)) {
+//         req.session.milk = 0;
+//     }
+//     req.session.milk = req.session.milk + 1000;
+//     res.send('session: ' + req.session.milk + '원');
+// });
+
+// app.post("/login",function(req,res){
+//     console.log("아이디 : "+req.body.userid);
+//     console.log("비밀번호 : "+req.body.userpw);
+//     res.send("로그인 되었습니다.");
+// });
+
+// app.get("/login",function(req,res){
+//     console.log(req.session);
+//     if(req.session.user){
+//         console.log("세션 유지");
+//         res.send('로그인 되었습니다.');
+//     }else{
+//         res.render("login.ejs");
+
+//     }
+// });
+
+app.get("/index", function(req, res) {
+    res.render("index.ejs", { user: req.session.user });
+});
+
+
+app.get("/login",function(req,res){
+    console.log(req.session);
+    if(req.session.user){
+        console.log("세션 유지");
+        res.render('index.ejs',{user:req.session.user});
+    }else{
+        res.render("login.ejs"); 
+    }
+});
+
+app.post('/login', (req, res) => {
+    console.log("아이디 : "+req.body.userid);
+    console.log("비밀번호 : "+req.body.userpw);
+
+    const userid = req.body.userid;
+    const userpw = req.body.userpw;
+
+    if (userid && userpw) {
+        let sql = "SELECT * FROM account WHERE userid = ? AND userpw = ?";
+        conn.query(sql, [userid, userpw], (err, results, fields) => {
+            if (err) {
+                res.send('데이터베이스 오류 발생: ' + err.message);
+                return;
+            }
+
+            if (results.length > 0) {
+                req.session.loggedin = true;
+                req.session.userid = userid;
+                req.session.user = results[0];
+                console.log("새로운 로그인");
+                res.render("index.ejs",{user:req.session.user}); 
+            } else {
+                res.render('login.ejs');
+            }
+        });
+    } else {
+        res.send('아이디와 비밀번호를 입력하세요!');
+    }
+});
+
+
+app.get("/logout", function(req, res) {
+    console.log('로그아웃');
+    req.session.destroy();
+    res.redirect("/index");
+});
+
+
+app.get("/signup",function(req,res){
+    res.render("signup.ejs");
+});
+
+
+app.post('/signup', (req, res) => {
+    console.log(req.body.userid); 
+    console.log(sha(req.body.userpw)); 
+    console.log(req.body.usergroup); 
+    console.log(req.body.useremail); 
+    const { userid, userpw, usergroup, useremail } = req.body;
+
+    if (userid && userpw && usergroup && useremail) {
+        let sql = "INSERT INTO account (userid, userpw, usergroup, useremail) VALUES (?, ?, ?, ?)";
+        conn.query(sql, [userid, userpw, usergroup, useremail], (err, results, fields) => {
+            if (err) {
+                res.send('회원가입 중 오류가 발생했습니다: ' + err.message);
+                return;
+            }
+            //res.send('회원가입이 완료되었습니다.');
+            res.redirect('/');
+        });
+    } else {
+        res.send('모든 항목를 입력하세요!');
+    }
+});
